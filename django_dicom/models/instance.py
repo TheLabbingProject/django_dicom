5139521e7a8a2070cc762c237f4f58696ec62ff5
Zvi Baratz <z.baratz@gmail.com>
2019-03-24 20:57:22 +0200

    Improved Series, Patient, and Study model creation, refactored a bit and moved the model managers to seperate files, added some help texts

--- django_dicom/models/instance.py
+++ django_dicom/models/instance.py
@@ -1,66 +1,15 @@
-import array
+import numpy as np
 import os
 import pydicom
 import shutil
-import zipfile
 
-from django.db.utils import IntegrityError
 from django.conf import settings
-from django.core.files.base import ContentFile
-from django.core.files.storage import default_storage
 from django.db import models
 from django.urls import reverse
-from django_dicom.models.dicom_entity import DicomEntity, DicomEntityManager
-from django_dicom.models.patient import Patient
-from django_dicom.models.series import Series
-from django_dicom.models.study import Study
+from django_dicom.models.dicom_entity import DicomEntity
+from django_dicom.models.managers import InstanceManager
 from django_dicom.models.validators import digits_and_dots_only
 from django_dicom.models.value_representation import parse_element
-
-
-class InstanceManager(DicomEntityManager):
-    UID_FIELD = "instance_uid"
-
-    def from_dcm(self, file_object):
-        content = ContentFile(file_object.read())
-        temp_file_name = default_storage.save("tmp.dcm", content)
-        instance = Instance(file=temp_file_name)
-        try:
-            instance.save()
-        except IntegrityError:
-            # If the UID exists already, delete the temporary file and return
-            # the existing instance
-            temp_file_path = os.path.join(settings.MEDIA_ROOT, temp_file_name)
-            uid = instance.get_entity_uid_from_headers(Instance)
-            os.remove(temp_file_path)
-            return Instance.objects.get_by_uid(uid)
-        return instance
-
-    def from_zip(self, file):
-        dest_name = default_storage.save("tmp.zip", ContentFile(file.read()))
-        dest_path = os.path.join(settings.MEDIA_ROOT, dest_name)
-        with zipfile.ZipFile(dest_path, "r") as archive:
-            for file_name in archive.namelist():
-                if file_name.endswith(".dcm"):
-                    with archive.open(file_name) as dcm_file:
-                        self.from_dcm(dcm_file)
-        os.remove(dest_path)
-
-    def from_local_directory(self, path: str):
-        for directory, sub_directory, file_list in os.walk(path):
-            for file_name in file_list:
-                if file_name.endswith(".dcm"):
-                    full_path = os.path.join(directory, file_name)
-                    with open(full_path, "rb") as f:
-                        self.from_dcm(f)
-
-
-def fix_slice_timing(value: bytes) -> list:
-    return [round(slice_time, 5) for slice_time in list(array.array("d", value))]
-
-
-def fix_gradient_direction(value: bytes) -> list:
-    return [float(value) for value in list(array.array("d", value))]
 
 
 class Instance(DicomEntity):
@@ -76,13 +25,10 @@ class Instance(DicomEntity):
     number = models.IntegerField(blank=True, null=True, verbose_name="Instance Number")
     date = models.DateField(blank=True, null=True)
     time = models.TimeField(blank=True, null=True)
-    b_value = models.IntegerField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    series = models.ForeignKey(Series, blank=True, null=True, on_delete=models.PROTECT)
-    study = models.ForeignKey(Study, blank=True, null=True, on_delete=models.PROTECT)
-    patient = models.ForeignKey(
-        Patient, blank=True, null=True, on_delete=models.PROTECT
+    series = models.ForeignKey(
+        "django_dicom.Series", blank=True, null=True, on_delete=models.PROTECT
     )
 
     objects = InstanceManager()
@@ -101,8 +47,11 @@ class Instance(DicomEntity):
     def get_absolute_url(self):
         return reverse("dicom:instance_detail", args=[str(self.id)])
 
-    def read_data(self) -> pydicom.dataset.FileDataset:
-        return pydicom.dcmread(self.file.path)
+    def read_file(self) -> pydicom.dataset.FileDataset:
+        return pydicom.read_file(self.file.path)
+
+    def get_data(self) -> np.ndarray:
+        return self.read_file().pixel_array
 
     def read_headers(self) -> pydicom.dataset.FileDataset:
         return pydicom.dcmread(self.file.path, stop_before_pixels=True)
@@ -166,62 +115,18 @@ class Instance(DicomEntity):
         header_key = model.FIELD_TO_HEADER.get(field_name)
         return self.get_header_value(header_key)
 
-    def get_related_entity_field_name(self, model: DicomEntity) -> str:
-        return model.__name__.lower()
-
-    def get_or_create_related_entity(self, model: DicomEntity) -> DicomEntity:
+    def get_or_create_by_uid(self, model: DicomEntity) -> DicomEntity:
         entity_uid = self.get_entity_uid_from_headers(model)
         return model.objects.get_or_create_by_uid(entity_uid)
 
-    def relate_entity(self, entity_instance: DicomEntity) -> bool:
-        model = type(entity_instance)
-        field_name = self.get_related_entity_field_name(model)
-        setattr(self, field_name, entity_instance)
-
-    def create_relations(self):
-        for entity in (Series, Patient, Study):
-            entity_instance, created = self.get_or_create_related_entity(entity)
-            self.relate_entity(entity_instance)
-
-    def get_related_by_type(self, entity_type: DicomEntity) -> DicomEntity:
-        field_name = self.get_related_entity_field_name(entity_type)
-        return getattr(self, field_name, None)
-
-    def associate_series_with_entity(self, model: DicomEntity):
-        related_entity = self.get_related_by_type(model)
-        self.series.relate_entity(related_entity)
-
-    def create_series_relations(self, force: bool = False):
-        for entity in (Patient, Study):
-            has_relation = self.series.has_relation(entity)
-            if not has_relation or force:
-                self.associate_series_with_entity(entity)
-                self.series.save()
-
-    def update_related_from_headers(self, force: bool = False):
-        for model in (Series, Patient, Study):
-            relation = getattr(self, model.__name__.lower(), None)
-            if isinstance(relation, model):
-                relation.update_fields_from_header(force=force)
-                relation.save()
-
-    def move_file(self, new_name: str = None) -> str:
-        """
-        Move the 'file' FileField attribute from its current location to
-        another location relative to MEDIA_ROOT.
-
-        :param new_name: New relative path, defaults to None (default path)
-        :param new_name: str, optional
-        :return:
-        :rtype: None
-        """
-        new_name = new_name or self.get_default_relative_path()
-        initial_path = self.file.path
-        self.file.name = new_name
-        new_path = os.path.join(settings.MEDIA_ROOT, self.file.name)
-        os.makedirs(os.path.dirname(new_path), exist_ok=True)
-        os.rename(initial_path, new_path)
-        return new_path
+    def move_file(self, relative_destination: str = None) -> str:
+        relative_destination = relative_destination or self.get_default_relative_path()
+        destination = os.path.join(settings.MEDIA_ROOT, relative_destination)
+        current_path = self.file.path
+        self.file.name = relative_destination
+        os.makedirs(os.path.dirname(destination), exist_ok=True)
+        os.rename(current_path, destination)
+        return destination
 
     def create_backup(self, dest: str):
         shutil.copyfile(self.file.path, dest)
@@ -244,6 +149,20 @@ class Instance(DicomEntity):
     @property
     def gradient_direction(self):
         return self.get_header_value(("0019", "100e"))
+
+    @property
+    def patient(self):
+        try:
+            return self.series.patient
+        except AttributeError:
+            return None
+
+    @property
+    def study(self):
+        try:
+            return self.series.study
+        except AttributeError:
+            return None
 
     class Meta:
         indexes = [
