6faa8ec9d35929d08de06a6a7a8312255b8f179d
Zvi Baratz <z.baratz@gmail.com>
2019-05-22 15:32:15 +0300

    WIP: Integration with Vue.js frontend

--- django_dicom/urls.py
+++ django_dicom/urls.py
@@ -12,6 +12,6 @@ router.register(r"patients", views.PatientViewSet)
 
 urlpatterns = [
     path("dicom/", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    # path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
 
