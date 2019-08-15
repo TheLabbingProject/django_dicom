b6eebf3744bc9a5fdb3d4bc17270dbdb54cd92c3
Zvi Baratz <z.baratz@gmail.com>
2019-08-15 16:53:42 +0300

    Removed old templates

--- django_dicom/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class CreateImagesForm(forms.Form):
-    dcm_files = forms.FileField(
-        label="DICOM files", widget=forms.ClearableFileInput(attrs={"multiple": True})
-    )
