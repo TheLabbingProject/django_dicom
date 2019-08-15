aa288c19cddb4ac5df294487b5383ae6e1d2e7ab
Zvi Baratz <z.baratz@gmail.com>
2019-08-15 16:51:56 +0300

    Changed API names from plural to singular

--- django_dicom/urls.py
+++ django_dicom/urls.py
@@ -4,10 +4,10 @@ from rest_framework import routers
 
 app_name = "dicom"
 router = routers.DefaultRouter()
-router.register(r"images", views.ImageViewSet)
+router.register(r"image", views.ImageViewSet)
 router.register(r"series", views.SeriesViewSet)
-router.register(r"studies", views.StudyViewSet)
-router.register(r"patients", views.PatientViewSet)
+router.register(r"study", views.StudyViewSet)
+router.register(r"patient", views.PatientViewSet)
 
 
 urlpatterns = [path("dicom/", include(router.urls))]
