db3bc607f9741404a9cb634c5ac3f18bb7b19967
Zvi Baratz <z.baratz@gmail.com>
2019-09-12 17:01:02 +0300

    Set version to 0.0.1 and added twine to to dev requirements before publishing

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("requirements/dev.txt") as fh:
 
 setup(
     name="django_dicom",
-    version="0.1.0",
+    version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
     license="MIT",
