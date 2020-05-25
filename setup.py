ff1e50f8c4b43beee935109a309cf72f1337bcde
Zvi Baratz <z.baratz@gmail.com>
2020-05-25 18:04:20 +0300

    Fixed requirements and added them to MANIFEST.in so that they're included in the source distribution.

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
-import os
 from setuptools import find_packages, setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
 with open("requirements.txt") as fh:
     requirements = fh.read().splitlines()
+    install_requires = [
+        requirement
+        for requirement in requirements
+        if not requirement.startswith("git+")
+    ]
+    dependency_links = [
+        requirement for requirement in requirements if requirement.startswith("git+")
+    ]
 
 with open("requirements-dev.txt") as fh:
     dev_requirements = fh.read().splitlines()
@@ -27,7 +31,8 @@ setup(
     author_email="baratzz@pm.me",
     keywords="django mri dicom dcm neuroimaging",
     python_requires=">=3.6",
-    install_requires=requirements,
+    install_requires=install_requires,
+    dependency_links=dependency_links,
     extras_require={"dev": dev_requirements},
     classifiers=[
         "Development Status :: 3 - Alpha",
