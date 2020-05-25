26b372fdb4da8f8340dbf2e6a68913e10c3cdd02
Zvi Baratz <z.baratz@gmail.com>
2020-05-25 18:23:25 +0300

    Updated test settings to get DB credentials from environment.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -28,8 +28,12 @@ MIDDLEWARE = [
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "django_dicom",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DB_NAME", "django_dicom"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "password"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", 5432),
     }
 }
 
