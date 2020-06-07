7ff874913bc7d5746491da2c420915539918bcca
Zvi Baratz <z.baratz@gmail.com>
2020-06-07 18:40:46 +0300

    Added docstrings to the models' logs module.

--- django_dicom/models/logs.py
+++ django_dicom/models/logs.py
@@ -1,3 +1,7 @@
+"""
+Log messages to be used by models.
+"""
+
 ENTITY_CREATION = "New {name} instance created."
 ENTITY_SAVE_START = "Saving {name} instance to the database..."
 ENTITY_SAVE_END = "{name} #{pk} successfully saved to the database."
