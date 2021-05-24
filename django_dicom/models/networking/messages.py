49f6b00a4381b2e4e1525561e12300fcfab30279
Zvi Baratz <z.baratz@gmail.com>
2021-05-24 15:30:25 +0300

    Messages fix.

--- django_dicom/models/networking/messages.py
+++ django_dicom/models/networking/messages.py
@@ -8,15 +8,9 @@ C_ECHO_HANDLING = "C-ECHO request handling enabled."
 PDU_LIMIT_CONFIGURATION = (
     "DICOM dataset transfer PDU size limited to {maximum_pdu_size} data units."
 )
-ASSOCIATION_SERVER_START = (
-    "Sending association request to DICOM storage service class user {user}..."
-)
-SERVER_START_SUCCESS = (
-    "SUCCESS! Association with {user} successfully established."
-)
-SERVER_START_ERROR = (
-    "Failed to start server with the following exception: {exception}"
-)
+SERVER_START = "Starting storage SCP at {user}..."
+SERVER_START_SUCCESS = "SUCCESS! Storage SCP server successfully started."
+SERVER_START_ERROR = "Failed to start storage SCP server with the following exception: {exception}"
 SERVER_NOT_CREATED = "Server instantiation failed with no exception! No new server found in the app's application entity."
 
 # flake8: noqa: E501
