d1cba1712515479c97f0f273582162167215e06e
Zvi Baratz <z.baratz@gmail.com>
2022-07-19 10:46:59 +0300

    Added today_only kwarg to import task.

--- django_dicom/tasks.py
+++ django_dicom/tasks.py
@@ -1,5 +1,6 @@
 import math
 from typing import Iterable, Union
+from datetime import datetime
 
 from celery import group, shared_task
 
@@ -7,7 +8,7 @@ from django_dicom.models.image import Image
 
 
 @shared_task(name="django_dicom.import-data")
-def import_data(path: Union[str, Iterable[str]], max_parallel: int = 5):
+def import_data(path: Union[str, Iterable[str]], max_parallel: int = 5, today_only: bool = False):
     """
     Imports new data (unfamiliar subdirectories) from the provided path.
 
@@ -15,6 +16,9 @@ def import_data(path: Union[str, Iterable[str]], max_parallel: int = 5):
     ----------
     data_directory : Union[int, str, Path]
         Directory path or DataDirectory instance ID
+    today_only : bool
+        Whether to look for a '<year>/<month>/<day>' subdirectory to import,
+        default is False
 
     Returns
     -------
@@ -22,8 +26,15 @@ def import_data(path: Union[str, Iterable[str]], max_parallel: int = 5):
         The names of the subdirectories within the DataDirectory that were
         imported
     """
-
-    if isinstance(path, str):
+    if isinstance(path, (str, Path)):
+        if today_only:
+            today = datetime.today()
+            path = (
+                Path(path)
+                / str(today.year)
+                / str(today.month)
+                / str(today.day)
+            )
         try:
             return Image.objects.import_path(path, pattern="*")
         except Exception:
